30fd5ef995a2763572e29ab4aac6575be444f5b1
Chris Garrett <cmgarOK@gmail.com>
2021-06-18 20:28:50 -0500

    Updated setup

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     },
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
